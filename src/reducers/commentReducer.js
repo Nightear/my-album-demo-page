decf7d7ac95ccb6f020ef4604e84c0921adeaa15
Nightear <ericcgkilo@gmail.com>
2017-12-31 16:44:28 +0800

    Update and rename contactReducer.js to commentReducer.js

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -3,14 +3,14 @@ import * as actionTypes from '../actions/actionTypes';
 export default (state = [], action) => {
     switch (action.type){
       
-      case actionTypes.CREATE_NEW_CONTACT:
+      case actionTypes.CREATE_NEW_COMMENT:
       return [
         ...state,
-        Object.assign({}, action.contact)
+        Object.assign({}, action.comment)
       ];
 
 
-      case actionTypes.REMOVE_CONTACT:
+      case actionTypes.REMOVE_COMMENT:
       return state.filter((data, i) => i !== action.id);
 
       
